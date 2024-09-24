59c83789899b831331c709c0d291ea41ee7825fe
Tiago <tiago.abreu.laibacas@gmail.com>
2024-09-24 18:00:41 +0100

    Fixes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const userController = require(".../controllers/UserController");
+const userController = require("../controllers/UserController.js");
+
 const {
   authMiddleware,
   authorizeRoles,
