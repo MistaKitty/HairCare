60422e54caf1aa4abfac94cd540e2b7c4d869eb4
Tiago <tiago.abreu.laibacas@gmail.com>
2024-09-26 21:47:23 +0100

    Forced update

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const serviceController = require("../controllers/serviceController");
+const {
+  authMiddleware,
+  authorizeRoles,
+} = require("../middleware/authMiddleware");
 
 router.get("/", serviceController.getAllServices);
 
-router.post("/", authorizeRoles("admin"), serviceController.createService);
+router.post(
+  "/",
+  authMiddleware,
+  authorizeRoles("admin"),
+  serviceController.createService
+);
 
-router.put("/:id", authorizeRoles("admin"), serviceController.updateService);
+router.put(
+  "/:id",
+  authMiddleware,
+  authorizeRoles("admin"),
+  serviceController.updateService
+);
 
-router.delete("/:id", authorizeRoles("admin"), serviceController.deleteService);
+router.delete(
+  "/:id",
+  authMiddleware,
+  authorizeRoles("admin"),
+  serviceController.deleteService
+);
 
 module.exports = router;
